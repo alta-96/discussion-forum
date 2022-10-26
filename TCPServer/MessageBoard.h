4f30b9a044ab890467e3a495aadd64bd480451f9
Alex Ashley <alex.ashley@3squared.com>
2022-10-26 08:34:13 +0100

    create message board singleton

--- TCPServer/MessageBoard.h
+++ TCPServer/MessageBoard.h
@@ -1,19 +1,24 @@
 #ifndef __MESSAGE_BOARD_H
 #define __MESSAGE_BOARD_H
+#include <string>
 #include <unordered_map>
+#include <vector>
 
-
-class MessageBoard
-{
-public:
-	static bool Post();
-	static bool List();
-	static bool Count();
-	static bool Read();
-	static bool Exit();
-
+// Singleton MessageBoard class
+class MessageBoard {
 private:
-	std::unordered_map<std::string, std::string[]> messageBoard;
-};
+    std::unordered_map<std::string, std::vector<std::string>> messageBoard;
+    MessageBoard();  
+    
+public:
+    static MessageBoard& GetInstance() {
+        static MessageBoard instance;
+        return instance;
+    }
 
+    unsigned int Post(const std::string topic, const std::string messageBody);
+    std::string List() const;
+    unsigned int Count(const std::string topic);
+    std::string Read(const std::string topic, unsigned int messageIndex);
+};
 #endif __MESSAGE_BOARD_H
